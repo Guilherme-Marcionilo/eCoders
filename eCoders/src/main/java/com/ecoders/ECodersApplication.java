31a313bed513e4dcb0c77cd38a024797bb8c1657
Gabriel Reis Ritter <gabrielrritter@hotmail.com>
2020-12-05 18:12:39 -0300

    primeiro commit

---
+++ eCoders/src/main/java/com/ecoders/ECodersApplication.java
@@ -0,0 +1,13 @@
+package com.ecoders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECodersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECodersApplication.class, args);
+	}
+
+}
---
+++ eCoders/src/test/java/com/ecoders/ECodersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecoders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECodersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
